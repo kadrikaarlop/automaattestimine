86d1326df50e929fcb7dd5bfb70bc21b360d5a26
kadrikaarlop <45164995+kadrikaarlop@users.noreply.github.com>
2019-03-07 11:28:43 +0200

    Update AddressBookSpec.js

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,1 +1,10 @@
-
+describe('Address Book'), function() {
+    it('should be able to add a contact', function() {
+        var addressBook `new AddressBook(),
+            thiContact `new Contact();
+        
+        addressBook.addContact(thisContact);
+        
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+});
