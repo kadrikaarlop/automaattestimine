566f806df9b17aabc7819d9d316deef7825b459a
kadrikaarlop <45164995+kadrikaarlop@users.noreply.github.com>
2019-03-13 09:44:05 +0200

    Update

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,18 @@
 function AddressBook() {
     this.contacts = [];
+    this.initialComplete = false;
 }
 
-AddressBook.prototype.addContact = function(contact) {
-    this.contacts.push(contact);
+AddressBook.prototype.getInitialContacts = function (cb) {
+    let self = this;
+
+    setTimeout(function () {
+        self.initialComplete = true;
+        if (cb){
+            return cb();
+        }
+        
+    }, 3);
+    
 }
 
-AddressBook.prototype.getContact = function(index) {
-    return this.contacts[index];
-}
