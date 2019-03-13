b89eb9e68e8542e65cb4c8847c92ab472cd1c667
kadrikaarlop <45164995+kadrikaarlop@users.noreply.github.com>
2019-03-13 09:32:34 +0200

    Update

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -5,5 +5,31 @@ describe('Address Book', function () {
     beforeEach(function(){
         addressBook = new AddressBook();
         thisContact = new Contact();
+    });
+    
+     it('should be able to add a contact', function () {
+     let addressBook = new AddressBook(),
+         thisContact = new Contact();
+
+     addressBook.addContact(thisContact);
+     expect(addressBook.getContact(0)).toBe(thisContact);
 
     });
+    
+     it('should be able to delete a contact', function () {
+        var addressBook = new AddressBook(),
+            thisContact = new Contact();
+
+      addressBook.addContact(thisContact);
+      addressBook.deleteContact(0);
+
+      expect(addressBook.getContact(0)).not.toBeDefined();
+    });
+
+    
+    
+    
+    
+    
+    
+});
